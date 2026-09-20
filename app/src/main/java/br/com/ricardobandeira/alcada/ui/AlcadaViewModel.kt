0f7a5438c07abbb3b6b1b1b81267f61afbd67030
rhbandeira-tech <rh.bandeira@gmail.com>
2026-09-20 03:50:25 -0300

    Reject stale dataset selection

--- app/src/main/java/br/com/ricardobandeira/alcada/ui/AlcadaViewModel.kt
+++ app/src/main/java/br/com/ricardobandeira/alcada/ui/AlcadaViewModel.kt
@@ -85,7 +85,7 @@ class AlcadaViewModel(application: Application) : AndroidViewModel(application) 
         }
     }
 
-    fun selectDataset(id: String) { _selectedDataset.value = id }
+    fun selectDataset(id: String) { if (datasets.value.any { it.id == id }) _selectedDataset.value = id else _importState.value = OperationState(error = "O conjunto de dados selecionado não está mais disponível.") }
     fun deleteDataset(id: String) { viewModelScope.launch { runCatching { repository.deleteDataset(id) }.onSuccess { if (_selectedDataset.value == id) _selectedDataset.value = null }.onFailure { _importState.value = OperationState(error = friendlyError(it)) } } }
 
     fun importFiles(uris: List<Uri>) {
