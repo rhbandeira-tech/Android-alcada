8a74076232634df2a5cf2019a20bd43f71f72478
rhbandeira-tech <rh.bandeira@gmail.com>
2026-09-20 02:43:59 -0300

    Support new candle geometry rules in evaluator

--- app/src/main/java/br/com/ricardobandeira/alcada/domain/BacktestEngine.kt
+++ app/src/main/java/br/com/ricardobandeira/alcada/domain/BacktestEngine.kt
@@ -79,7 +79,7 @@ object BacktestEngine {
             "upperWick" -> feature.upperWick
             "lowerWick" -> feature.lowerWick
             "momentum" -> feature.momentum
-            "range" -> feature.range
+            "range" -> feature.range\n            "bodyRangeRatio" -> feature.bodyRangeRatio\n            "closeLocation" -> feature.closeLocation
             else -> return false
         }
         return when (rule.operator) { ">" -> value > rule.threshold; ">=" -> value >= rule.threshold; "<" -> value < rule.threshold; "<=" -> value <= rule.threshold; else -> false }
